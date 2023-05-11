c6dc442e38f3a6f63f80dcdaea72279c34d8cc5b
Facundo Herrera <facundhfed@gmail.com>
2023-05-11 17:12:52 +0200

    tsconfig checked

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,9 @@
-"use strict";
-console.log("Hola");
+(() => {
+    let myCustomCharacter = {
+        name: "Bruna",
+        powers: ["Let me Happy"]
+    };
+    console.log(myCustomCharacter);
+})();
+export {};
 //# sourceMappingURL=index.js.map
-
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hola");
+import { Character } from './Types/Character';
+(() => {
+
+    let myCustomCharacter: Character = {
+        name:"Bruna",
+        powers: ["Let me Happy"]
+    }
+    console.log(myCustomCharacter);
+
+})()
