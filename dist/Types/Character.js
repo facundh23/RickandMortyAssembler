7e60b93c2cdc12bd20f087559388838f50eab81f
Facundo Herrera <facundhfed@gmail.com>
2023-05-18 18:05:22 +0200

    Project checked

--- dist/Types/Character.js
+++ dist/Types/Character.js
@@ -1,2 +1,15 @@
+var statusCharacter;
+(function (statusCharacter) {
+    statusCharacter["alive"] = "Alive";
+    statusCharacter["dead"] = "Dead";
+    statusCharacter["unkown"] = "Unkown";
+})(statusCharacter || (statusCharacter = {}));
+var genderCharacter;
+(function (genderCharacter) {
+    genderCharacter["female"] = "Female";
+    genderCharacter["male"] = "Male";
+    genderCharacter["genderless"] = "Genderless";
+    genderCharacter["unknown"] = "unknown";
+})(genderCharacter || (genderCharacter = {}));
 export {};
 //# sourceMappingURL=Character.js.map
--- src/Types/Character.ts
+++ src/Types/Character.ts
@@ -1,11 +1,24 @@
+enum statusCharacter {
+    alive = "Alive", 
+    dead = "Dead",
+    unkown = "Unkown"
+}
+
+enum genderCharacter {
+    female= 'Female',
+    male =  'Male', 
+    genderless= 'Genderless',
+    unknown = 'unknown'
+}
+
 export type Character = { 
     id?: number, 
     name?: string, 
-    status?: string, 
+    status?: statusCharacter, 
     species?: string,
     location: string
     type?:string,
-    gender?:string
+    gender?:genderCharacter
     origin?:object
     episode?:string,
     characters?:string[],
